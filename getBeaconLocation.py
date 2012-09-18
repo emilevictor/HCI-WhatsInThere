e416240be1b8f25ba4d1e46a5417706b9ab7f57a
Michael Mallon <michael@Alendil.(none)>
2012-09-18 17:13:42 +1000

    Now generates random building

--- getBeaconLocation.py
+++ getBeaconLocation.py
@@ -1,7 +1,10 @@
 from mod_python import apache
+import random
 
 def handler(req):
-	req.content_type="text/html"
+	req.content_type="application/json"
 	req.send_http_header()
-	req.write("Hello World!")
+	buld = random.int(1,100)
+	room = rand.int(1,8)*100 + rand.int(1,50)
+	req.write('{ "room" : ' +repr(room) + ', "building": '+repr(buld) +'}')
 	return apache.OK
