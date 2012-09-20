78e53c3370d5bf69ec17928303e57c6f49b2f04b
Michael Mallon <michael@Alendil.(none)>
2012-09-20 19:00:26 +1000

    Fixed bug in getBeaconLocation dummy function

--- getBeaconLocation.py
+++ getBeaconLocation.py
@@ -1,10 +1,16 @@
 from mod_python import apache
 import random
+import simplejson
+
+class room():
+	room = None
+	buld = None
 
 def handler(req):
 	req.content_type="application/json"
 	req.send_http_header()
-	buld = random.randint(1,100)
-	room = random.randint(1,8)*100 + random.randint(1,50)
-	req.write('{ "room" : ' +repr(room) + ', "building": '+repr(buld) +'}')
+	a = room()
+	a.buld = random.randint(1,100)
+	a.room = random.randint(1,8)*100 + random.randint(1,50)
+	req.write("'{'buld':%s,'room':%s}'"%(a.buld, a.room))
 	return apache.OK
