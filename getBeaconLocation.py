895ec3fc7e5671da58e959a8def02139ecc474e1
Michael Mallon <michael@Alendil.(none)>
2012-09-27 14:38:34 +1000

    Fix json object

--- getBeaconLocation.py
+++ getBeaconLocation.py
@@ -9,7 +9,7 @@ def getRoom(req,btaddr=None):
 	req.send_http_header()
 	buld = random.randint(1,100)
 	room = random.randint(1,8)*100 + random.randint(1,50)
-	req.write("'{'buld': %s,'room': %s}'"%(buld, room))
+	req.write("{'buld': %s,'room': %s}"%(buld, room))
 
 def getImage(req,buld=1):
 	req.content_type="image/png"
