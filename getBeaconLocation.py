8a1ac56c2b93f40a452869db0f33657693b32c57
Michael Mallon <michael@Alendil.(none)>
2012-09-27 14:23:58 +1000

    Reverted to JSON calls

--- getBeaconLocation.py
+++ getBeaconLocation.py
@@ -8,9 +8,17 @@ def getRoom(req,btaddr=None):
 	req.send_http_header()
 	buld = random.randint(1,100)
 	room = random.randint(1,8)*100 + random.randint(1,50)
-	req.write("var fluffypengins={'buld':%s,'room':%s};"%(buld, room))
+	req.write("'{'buld':%s,'room':%s}'"%(buld, room))
 
 def getImage(req,buld=1):
 	req.content_type="image/png"
 	req.send_http_header()
 	return m.getBuldImage(buld)
+
+
+def getClasses(req,buld=None,room=None):
+	req.content_type="application/json"
+	req.send_http_header()
+	if buld is None or room is None:
+		return "classypengins = null;"
+	return "null"
